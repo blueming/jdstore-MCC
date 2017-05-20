ff92d2243f371ebdcf0d49dabaa722deecfda623
blueming <wenyiff66@gmail.com>
2017-05-20 15:53:01 +0800

    add checkout routing

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,17 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
+
   resources :products do
     member do
       post :add_to_cart
     end
   end
+
   resources :carts do
     collection do
       delete :clean
+      post :checkout
     end
   end
   resources :cart_items
